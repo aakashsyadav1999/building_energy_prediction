ccba8d692a9e579cf94c085d5d450110e92a5ec6
Aakash Yadav <aakashsyadav1999@gmail.com>
2024-06-09 12:16:11 +0530

    constant_file

---
+++ src/constants/__init__.py
@@ -0,0 +1,8 @@
+import os
+from datetime import datetime
+
+TIMESTAMP: str = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+
+ARTIFACTS_DIR = os.path.join("artifacts")
+LOGS_DIR = "logs"
+LOGS_FILE_NAME = "SIDFC.log"
